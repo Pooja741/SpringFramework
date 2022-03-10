c539ff706cb29336c2599387a38a71f6d68f81d7
Pooja S <pooja.s@thoughtworks.com>
2022-03-10 22:09:52 +0530

    Setter injection through properties file

--- src/com/practice/springdemo/dependencyInjection/CricketCoach.java
+++ src/com/practice/springdemo/dependencyInjection/CricketCoach.java
@@ -5,7 +5,25 @@ import com.practice.springdemo.inversionOfControl.Coach;
 public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
+    private String team;
 
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
     public void setFortuneService(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
--- src/com/practice/springdemo/setterInjectionDemoApp.java
+++ src/com/practice/springdemo/setterInjectionDemoApp.java
@@ -1,5 +1,6 @@
 package com.practice.springdemo;
 
+import com.practice.springdemo.dependencyInjection.CricketCoach;
 import com.practice.springdemo.inversionOfControl.Coach;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,10 +8,12 @@ public class setterInjectionDemoApp {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Coach coach = context.getBean("cricketCoach", Coach.class);
+        CricketCoach coach = context.getBean("cricketCoach", CricketCoach.class);
 
         System.out.println(coach.getDailyFortune());
         System.out.println(coach.getDailyWorkout());
+        System.out.println(coach.getEmail());
+        System.out.println(coach.getTeam());
 
         context.close();
 
